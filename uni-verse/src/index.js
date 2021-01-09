cac61af89d2a92f13626ccd3fcae21ffe26d1a94
Surabhi Gupta <surabhi2029@gmail.com>
2021-01-08 18:42:40 -0800

    Saving changes

--- uni-verse/src/index.js
+++ uni-verse/src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-<<<<<<< HEAD
 import Firebase, { FirebaseContext } from '../src/Components/Firebase';
 
 ReactDOM.render(
@@ -11,11 +10,4 @@ ReactDOM.render(
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root'),
-=======
-
-ReactDOM.render(
-    <App />,
-    
-  document.getElementById('root')
->>>>>>> 1c4da088d06b16dbf140d0a57b14c462c694cf02
 );
