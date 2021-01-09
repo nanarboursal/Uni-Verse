d0ad1969ca51b77a4df7d30295d3cd4c830286a8
Nanar Boursalian <nanar.boursalian01@gmail.com>
2021-01-08 20:43:49 -0800

    Added log in and sign up pages

--- uni-verse/src/App.js
+++ uni-verse/src/App.js
@@ -1,19 +1,27 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
 import Homepage from './Components/Home/Homepage';
+import SignUpPage from "./Pages/SignUp/SignUp";
+import SignInPage from "./Pages/Login/Login";
 import Button from './Buttons.js';
-import React from 'react';
-import { Route } from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <React.Fragment>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/questions" component={Button} />
-      </React.Fragment>
-      <div className="App">
-        {/* <Homepage /> */}
-      </div>
-    </>
+    <Router>
+    <div>
+      <Route exact path={ROUTES.HOMEPAGE} component={Homepage} />
+      <Route exact path={ROUTES.SIGNUP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGNIN} component={SignInPage} />
+      <Route exact path="/questions" component={Button} />
+    </div>
+  </Router>
   );
 }
 
