af6a67e23ec1440b187927e3dd29d6d9c04475bd
Seema Vora <seema.s.vora@gmail.com>
2021-01-09 17:17:57 -0800

    Finishes the algorithm

--- uni-verse/src/CreateUserData/matchUser.py
+++ uni-verse/src/CreateUserData/matchUser.py
@@ -24,12 +24,33 @@ userMajor = (sampleUser['userMajor'])
 if(majorPref == 'yes major'):
   for i in range (len(obj['user_details'])):
     if str(obj['user_details'][i]['userMajor']) == userMajor:
-      # print(str(obj['user_details'][i]['userMajor']))
-      # print(newobj)
       temp = obj['user_details'][i]
       newobj.append(temp)
-      # print (newobj['user_details'][i])
-print(newobj)
-# print(str(obj['user_details'][2]['userMajor']))    
-# print(len(obj['user_details']))
-# print(obj)
+
+matches = []
+score = 0
+for i in range (len(newobj)):
+  if newobj[i]['gamer'] == sampleUser['gamer']:
+    score = score + 1
+  if newobj[i]['gender'] == sampleUser['gender']:
+    score = score + 1
+  if newobj[i]['hobby'] == sampleUser['hobby']:
+    score = score + 1
+  if newobj[i]['smoke'] == sampleUser['smoke']:
+    score = score + 1
+  if newobj[i]['social'] == sampleUser['social']:
+    score = score + 1
+  if score > 3:
+    matches.append(newobj[i])
+print(matches)
+
+
+def write_json(data, filename='matches.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+with open('matches.json') as jsonfile:
+    data = json.load(jsonfile)
+    temp = data['matchList']
+    temp.append(matches)
+write_json(data)
