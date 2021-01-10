be8f1defb356d798d2ebb32708a0bc0ad00fa64d
Nanar Boursalian <nanar.boursalian01@gmail.com>
2021-01-09 16:07:59 -0800

    Added creator cards to homepage

--- uni-verse/src/Components/Firebase/firebase.js
+++ uni-verse/src/Components/Firebase/firebase.js
@@ -8,13 +8,6 @@ const config = {
   // projectId: YOUR_PROJECT_ID,
   // storageBucket: '',
   // messagingSenderId: YOUR_MESSAGING_SENDER_ID,
-  apiKey: "AIzaSyDm5bO5HeO0Vnl5eUueqYZOSfic4o48cC4",
-  authDomain: "uni-verse-scedemo.firebaseapp.com",
-  projectId: "uni-verse-scedemo",
-  storageBucket: "uni-verse-scedemo.appspot.com",
-  messagingSenderId: "1085241479794",
-  appId: "1:1085241479794:web:530968a5c736478893e8eb",
-  measurementId: "G-ZQ9MP8XRPZ"
 };
 
 export default class Firebase {
--- uni-verse/src/Components/Home/Homepage.js
+++ uni-verse/src/Components/Home/Homepage.js
@@ -4,9 +4,43 @@ import logo2 from "./logo2.png";
 import { Link, BrowserRouter as Router } from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
 import { AuthUserContext } from "../Session";
+import CreatorCard from "./CreatorCard";
+import nanar from "./nanar.jpeg";
+import seema from "./seema.jpg";
+import surabhi from "./surabhi.png";
 
 export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      creators: [
+        {
+          name: "Nanar Boursalian",
+          Linkedin: "https://www.linkedin.com/in/nanarboursalian/",
+          pic: nanar
+        },
+        {
+          name: "Seema Vora",
+          Linkedin: "https://www.linkedin.com/in/seemasvora/",
+          pic: seema
+        },
+        {
+          name: "Surabhi Gupta",
+          Linkedin: "https://www.linkedin.com/in/gupta-surabhi/",
+          pic: surabhi
+        }
+      ]
+    };
+  }
+
   render() {
+    let creatorCards = this.state.creators.map(creators => {
+      return (
+        <li className="flex-item">
+          <CreatorCard creators={creators} />
+        </li>
+      )
+    })
     return (
       <>
         <Router></Router>
@@ -44,11 +78,22 @@ export default class Homepage extends Component {
                   <Link className="sign-up" to={ROUTES.SIGNUP}>
                     Sign Up Now!
                   </Link>
-                  <p className="log-in-link" >Already have an account? <Link style={{color: "blue"}} to={ROUTES.SIGNIN}>Log In.</Link></p>
+                  <p className="log-in-link">
+                    Already have an account?{" "}
+                    <Link style={{ color: "blue" }} to={ROUTES.SIGNIN}>
+                      Log In.
+                    </Link>
+                  </p>
                 </React.Fragment>
               )
             }
           </AuthUserContext>
+        </div>
+        <div className="creators-box">
+          <h1>Meet the Creators!</h1>
+          <ul className="flex-container wrap-reversez">
+            {creatorCards}
+          </ul>
         </div>
       </>
     );
